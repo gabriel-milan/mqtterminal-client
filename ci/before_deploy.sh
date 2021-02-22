cd5fb63ab396bf37a69ef3170577f4d951dba1ab
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-02-22 20:25:41 -0300

    Fix deploy script

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,7 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin $CRATE_NAME --target $TARGET --release -- -C lto
+    cross rustc --bin $CRATE_NAME --target $TARGET --release --features vendored-openssl -- -C lto
 
     cp target/$TARGET/release/$CRATE_NAME $stage/
 
